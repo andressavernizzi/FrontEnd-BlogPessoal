1ab92dfbf7873d1513ee7623a4ca3fd73e3aee5b
Andressa Vernizzi <andressavernizzi@hotmai.com>
2023-11-09 15:07:29 -0300

    atualizar metodo CRUD de temas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,15 +15,17 @@ function Navbar() {
     }
     return (
         <>
-            <div className="container flex justify-between text-lg">
-                <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
+            <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+                <div className="container flex justify-between text-lg ">
+                    <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
 
-                <div className='flex gap-4'>
-                    Postagens
-                    Temas
-                    Cadastrar tema
-                    Perfil
-                    <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    <div className='flex gap-4'>
+                        Postagens
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastroTema' className='hover:underline'>Cadastrar Tema</Link>
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
                 </div>
             </div>
         </>
